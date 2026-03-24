f82bd50bc16eda2189dd3b8a638d2ff75cfae39c
Crax <55369708+KT-KX@users.noreply.github.com>
2026-03-24 12:44:02 +0800

    Update AIT2209937.py

--- AIT2209937.py
+++ AIT2209937.py
@@ -1,1 +1,19 @@
 
+# app.py - The code the Agent needs to check
+
+def process_user_name(name):
+    """
+    Capitalizes the first letter of the name.
+    BUG: If the name is an empty string, name[0] will throw an IndexError.
+    """
+    if name is None:
+        return "Guest"
+    
+    # This line will crash if name == ""
+    formatted_name = name[0].upper() + name[1:].lower()
+    return formatted_name
+
+if __name__ == "__main__":
+    print(process_user_name("uncle")) # Works
+    print(process_user_name(""))    # WILL CRASH
+    print("halo halo")
